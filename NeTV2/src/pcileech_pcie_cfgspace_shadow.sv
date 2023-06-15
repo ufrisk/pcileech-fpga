f664931b2046816eff5fef1e27d8d22229824257
ekknod <ekknod@gmail.com>
2023-06-16 02:00:49 +0300

    fix for git: ekknod/drvscan scan_pcileech

--- NeTV2/src/pcileech_pcie_cfgspace_shadow.sv
+++ NeTV2/src/pcileech_pcie_cfgspace_shadow.sv
@@ -34,7 +34,7 @@ module pcileech_pcie_cfgspace_shadow(
     wire            bram_wr_2_usb = ~bram_wr_1_tlp & dshadow2fifo.rx_wren;
     wire            bram_wr_3_int = ~bram_wr_1_tlp & ~bram_wr_2_usb & int_wren;
     wire [3:0]      bram_wr_be = bram_wr_1_tlp ? dshadow2tlp.rx_be : (bram_wr_2_usb ? dshadow2fifo.rx_be : (bram_wr_3_int ? 4'b1111 : 4'b0000));
-    wire [9:0]      bram_wr_addr = bram_wr_1_tlp ? dshadow2tlp.rx_addr : (bram_wr_2_usb ? dshadow2fifo.rx_addr : int_wr_addr);
+    wire [9:0]      bram_wr_addr = bram_wr_1_tlp ? 10'b0000000000 : (bram_wr_2_usb ? dshadow2fifo.rx_addr : int_wr_addr);
     wire [31:0]     bram_wr_data = bram_wr_1_tlp ? dshadow2tlp.rx_data : (bram_wr_2_usb ? dshadow2fifo.rx_data : int_wr_data);
     
     // ----------------------------------------------------------------------------
